602762cc46b8657c3698baee90a82fc28eae9bff
Milan Bakic <bakicm.95@gmail.com>
2018-05-24 17:54:59 +0200

    F commit

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'role_structure' => [
+        'Doctor' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'administrator' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'user' => [
+            'profile' => 'r,u'
+        ],
+    ],
+    'permission_structure' => [],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
